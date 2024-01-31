fa05da03e412d96892349e5e94cdd3b84262982b
JohnNesha <150056202+JohnNesha@users.noreply.github.com>
2024-01-30 22:54:31 -0500

    Add Cost Calculator sln

---
+++ CostCalulator/CostCalulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CostCalulator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Tile Cost Calculator. Use this calculator to calulate the total amount of flooring needed. \n");
+    }
+}
+
